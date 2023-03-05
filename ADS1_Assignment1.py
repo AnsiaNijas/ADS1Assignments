a2270f9d80dd0fc06c38eba04a7c1eb513907601
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2023-03-05 00:29:29 +0000

    Added Stacked Bar pot code

--- ADS1_Assignment1.py
+++ ADS1_Assignment1.py
@@ -24,6 +24,11 @@ def readPiePlotData():
   return df_Food_Prices_updated
 
 
+def readStackedBarPlotData():
+    df_illiterate_population = pd.read_csv("Youth illiterate population.csv")
+    return df_illiterate_population
+
+
 def linePlot(df):
     df_Food_Prices_updated = df
     plt.figure()
@@ -41,8 +46,9 @@ def linePlot(df):
     plt.xlim(2017, 2020)
     plt.xticks([2017, 2018, 2019, 2020], [2017, 2018, 2019, 2020])
     plt.xlabel("Year")
-    plt.ylabel("Number of People (millions")
-    plt.title("Millions of people unable to afford a healthy diet")
+    plt.ylabel("Population (millions")
+    plt.title(
+        "Population who cannot afford a healthy diet in past 4 years for different countries")
     plt.legend(loc='upper right')
     plt.savefig('LinePlot.jpg')
     plt.show()
@@ -54,9 +60,30 @@ def piePlot(df):
     names = ["High income [HIC]", "Low income [LIC] ", "Lower middle income [LMC]",
              "Upper middle income [UMC]"]
     plt.figure()
-    plt.pie(food_prices['2020.0'], labels=names)
+    plt.subplot(2, 1, 1)
+    plt.pie(food_prices['2019.0'], autopct='%1.1f%%', labels=names)
     plt.title("Population who cannot afford a healthy diet (millions) in 2020")
+    plt.subplot(2, 1, 2)
+    plt.pie(food_prices['2020.0'], autopct='%1.1f%%', labels=names)
+    plt.title("Population who cannot afford a healthy diet (millions) in 2019")
+    plt.tight_layout()
     plt.savefig("PiePlot.png")
+    plt.show()
+    return
+
+
+def stackedBarPlot(df):
+    illiterate_population = df
+    plt.figure()
+    plt.bar(illiterate_population["Time"],
+            illiterate_population["Youth illiterate population, 15-24 years, male (number) [UIS.LP.AG15T24.M]"], width=.5, label="Male")
+    plt.bar(illiterate_population["Time"], illiterate_population["Youth illiterate population, 15-24 years, female (number) [UIS.LP.AG15T24.F]"],
+            bottom=illiterate_population["Youth illiterate population, 15-24 years, male (number) [UIS.LP.AG15T24.M]"], width=.5, label="Female")
+    plt.title("Youth illiterate population")
+    plt.xlabel("Year")
+    plt.ylabel("Population (millions)")
+    plt.legend()
+    plt.savefig("StackedBarPlot.png")
     plt.show()
     return
 
@@ -64,3 +91,4 @@ def piePlot(df):
 if __name__ == "__main__":
     linePlot(readLinePlotData())
     piePlot(readPiePlotData())
+    stackedBarPlot(readStackedBarPlotData())
